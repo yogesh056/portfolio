e889a5aa74eed88a618e6d73ac8adbd75ad49092
Yogeshwaran <yogeshwaran@Yogeshwarans-MacBook-Pro.local>
2022-01-09 12:13:46 +0530

    adds github icon

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -2,20 +2,24 @@ import React from "react";
 const instagram = require("../assets/instagram.png");
 const facebook = require("../assets/facebook.png");
 const linkedin = require("../assets/linkedin.png");
-const bitbucket = require("../assets/bitbucket.png");
+const github = require("../assets/github.png");
 export default function SocialLinks() {
   return (
     <div className="social-links ">
-      <a href="" className="align-center">
-        <img className={"social-icons"} src={bitbucket} />
+      <a href="https://github.com/yogesh056/" className="align-center" target="_blank">
+        <img className={"social-icons"} src={github} />
       </a>
-      <a href="" className="align-center">
-        <img className={"social-icons"} src={instagram} />
-      </a>
-      <a href="" className="align-center">
+      <a href="https://www.linkedin.com/in/yogeshwaran-ramesh-43978016b/" className="align-center" target="_blank">
         <img className={"social-icons"} src={linkedin} />
       </a>
-      <a href="https://www.facebook.com/yogesh056/" className="align-center">
+      <a href="https://www.instagram.com/yoge.exe/" className="align-center" target="_blank">
+        <img className={"social-icons"} src={instagram} />
+      </a>
+      <a
+        href="https://www.facebook.com/yogesh056/"
+        target="_blank"
+        className="align-center"
+      >
         <img className={"social-icons"} src={facebook} />
       </a>
     </div>
